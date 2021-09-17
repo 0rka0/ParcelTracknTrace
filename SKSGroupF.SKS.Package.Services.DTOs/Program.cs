2660c0958b6cf3a65e0dc63e35648522e4b066fc
David Friedwagner <david.fw3004@gmail.com>
2021-09-17 08:41:40 +0200

    init commit

---
+++ SKSGroupF.SKS.Package.Services.DTOs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SKSGroupF.SKS.Package.Services.DTOs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
