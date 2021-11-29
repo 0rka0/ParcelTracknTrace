1d5c87d8cb8d6a9b3f910d79c1e815478089229b
David Friedwagner <david.fw3004@gmail.com>
2021-11-29 15:28:07 +0100

    webhook setup

---
+++ src/Webhooks/SKSGroupF.SKS.Package.Webhooks.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SKSGroupF.SKS.Package.Webhooks.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
