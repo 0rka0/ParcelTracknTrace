91641a22c281456a41420d508f9624da5ef5bb62
FFilipR <filip@gmail.com>
2021-12-01 16:20:35 +0100

    Integration tests for Warehouse and Parcel added

---
+++ src/SKS.GroupF.SKS.Package.IntegrationTest/ParcelTests.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SKSGroupF.SKS.Package.Services.DTOs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKS.GroupF.SKS.Package.IntegrationTest
+{
+    class TrackingID
+    {
+        public string trackingId { get; set; }
+    }
+    public class ParcelTests
+    {
+        private string url;
+        private HttpClient client;
+        private TrackingID trackingId;
+
+        [SetUp]
+        public void Setup()
+        {
+            url = TestContext.Parameters.Get("url", "https://sksgroupf-webapp.azurewebsites.net");
+
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+
+        [Test, Description("Submitting a parcel")]
+        public async Task SubmitParcel()
+        {
+            Console.WriteLine($"Calling {url}/parcel");
+
+            StreamReader reader = new StreamReader("../../../testparcel.json");
+            string fileContent = reader.ReadToEnd();
+            var content = new StringContent(fileContent, Encoding.UTF8, "application/json");
+
+            var res = await client.PostAsync("/parcel", content);
+
+            var jsonBody = await res.Content.ReadAsStringAsync();
+            trackingId = JsonConvert.DeserializeObject<TrackingID>(jsonBody);
+            
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, res.StatusCode, "Status Code not 200-OK");
+            Assert.AreEqual("application/json", res.Content.Headers.ContentType.MediaType, "Mediatype not application/json");
+            Assert.IsNotEmpty(jsonBody, "No body returned");
+
+
+            var resMsg = await client.GetAsync($"/parcel/{trackingId.trackingId}");
+            var jsonBody2 = await resMsg.Content.ReadAsStringAsync();
+
+            var trackingInfo = JsonConvert.DeserializeObject<TrackingInformation>(jsonBody2);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, resMsg.StatusCode, "Status Code not 200-OK");
+            Assert.AreEqual("application/json", resMsg.Content.Headers.ContentType.MediaType, "Mediatype not application/json");
+            Assert.IsNotEmpty(jsonBody2, "No body returned");
+            Assert.AreEqual(TrackingInformation.StateEnum.PickupEnum, trackingInfo.State); ;
+        }
+
+    }
+
+    
+}
---
+++ src/SKS.GroupF.SKS.Package.IntegrationTest/WarehouseTests.cs
@@ -0,0 +1,78 @@
+using Newtonsoft.Json;
+using NUnit.Framework;
+using SKSGroupF.SKS.Package.Services.DTOs.Models;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SKS.GroupF.SKS.Package.IntegrationTest
+{
+    public class WarehouseTests
+    {
+        private string url;
+        private HttpClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            url = TestContext.Parameters.Get("url", "https://sksgroupf-webapp.azurewebsites.net");
+
+            client = new HttpClient()
+            {
+                BaseAddress = new Uri(url)
+            };
+        }
+
+        [Test, Description("Importing Warehouse Tree")]
+        public async Task ImportWarehouse()
+        {
+            Console.WriteLine($"Calling {url}/Warehouse");
+            StreamReader reader = new StreamReader("../../../trucks-new2-light-transferwh.json");
+            string fileContent = reader.ReadToEnd();
+            var content = new StringContent(fileContent, Encoding.UTF8, "application/json");
+
+            var res = await client.PostAsync("/Warehouse", content);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, res.StatusCode, "Status Code not 200-OK");
+
+            var resMsg = await client.GetAsync("/Warehouse");
+            var jsonBody = await resMsg.Content.ReadAsStringAsync();
+
+            Assert.That(jsonBody, Contains.Substring("Warehouse Level 1 - Wien"), "Sample Text not within the JSON");
+        }
+         
+        [Test, Description("Exporting Warehouse Tree")]
+        public async Task ExportWarehouse()
+        {
+            Console.WriteLine($"Calling {url}/Warehouse");
+            var res = await client.GetAsync("/Warehouse");
+            var jsonBody = await res.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, res.StatusCode, "Status Code not 200-OK");
+            Assert.AreEqual("application/json", res.Content.Headers.ContentType.MediaType, "Mediatype not application/json");
+            Assert.IsNotEmpty(jsonBody, "No body returned");
+            Assert.That(jsonBody, Contains.Substring("Warehouse Level 1 - Wien"), "Sample Text not within the JSON");
+        }
+
+
+        [Test, Description("Getting Warehouse Tree")]
+        public async Task GetSingleWarehouseByCode()
+        {
+            var code = "WTTA080";
+            Console.WriteLine($"Calling {url}/Warehouse/{code}");
+
+            var res = await client.GetAsync($"/Warehouse/{code}");
+            var jsonBody = await res.Content.ReadAsStringAsync();
+            var truck = JsonConvert.DeserializeObject<Truck>(jsonBody);
+
+            Assert.AreEqual(System.Net.HttpStatusCode.OK, res.StatusCode, "Status Code not 200-OK");
+            Assert.AreEqual("application/json", res.Content.Headers.ContentType.MediaType, "Mediatype not application/json");
+            Assert.IsNotEmpty(jsonBody, "No body returned");
+            Assert.AreEqual(code, truck.Code);
+        }
+
+
+    }
+}
